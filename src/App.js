1de14e7dac92de84e5b89603ac3ed6685d921dcc
Lakshin_Pathak <145486751+LakshinPathak@users.noreply.github.com>
2024-12-22 15:58:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Login from './Login';
-// import ChatArea from './ChatArea';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/chat" element={<ChatArea />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 
 
 import React from 'react';
